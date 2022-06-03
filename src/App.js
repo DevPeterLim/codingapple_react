1c2d57656ebfd9ff95cd364debdf349b89459d5d
DevPeterLim <dev.peter.lim@gmail.com>
2022-06-03 14:30:24 +0900

    map 메서드 활용중 오류 수정중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [title, setTitle] = useState(['제목1', '남자코트추천', '포은대로 맛집'])
+  const [like, setLike] = useState([0, 10, 0]);
+  const [modal, setModal] = useState(false)
+  let likeCopy = [...like];
+  // function handleLike(i){
+  //   setLike(like[i]+1)
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='TopNav'>
+        <h4>Blog</h4>
+      </div>
+      <button onClick={()=>{
+        let copy = [...title];
+        copy.sort();
+        setTitle(copy)
+        }}>가나다순 정렬</button>
+
+      {
+        title.map((elem, i)=> {
+        return (
+          <div className='list' key={i}>
+            <h4>{elem}
+              <span onClick={()=>{setLike(like[i]+1)}}>👍🏻</span> {like[i]}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>    
+        )})
+        }
+      {/* <div className='list'>
+        <h4 onClick={()=>{
+          let copy = [...title];
+          copy[0] = "여자코트추천";
+          setTitle(copy);
+        }}>{title[0]}<span onClick={handleLike}>👍🏻</span> {like} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4 onClick={()=>{setModal(!modal)}}>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div> */}
+      {
+        modal == true ? <Modal/> : null
+      }
     </div>
   );
+}
+const Modal = () => {
+  return (
+  <div className='modal'>
+    <h4>제목</h4>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
+  )
 }
 
 export default App;
