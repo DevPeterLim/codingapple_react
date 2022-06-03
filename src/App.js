5de93f6a30093d711535fea091732217ad9eec2e
DevPeterLim <dev.peter.lim@gmail.com>
2022-06-03 20:15:07 +0900

    수정완료

--- src/App.js
+++ src/App.js
@@ -8,61 +8,73 @@ function App() {
   const [title, setTitle] = useState(['제목1', '남자코트추천', '포은대로 맛집'])
   const [like, setLike] = useState([0, 10, 0]);
   const [modal, setModal] = useState(false)
-  let likeCopy = [...like];
-  // function handleLike(i){
-  //   setLike(like[i]+1)
-  // }
+  const [titleNum, setTitleNum] = useState(0);
+  const [inputVal, setInputVal] = useState('');
+  function handleTitleSort(){
+    let copy = [...title];
+    copy.sort();
+    setTitle(copy)
+  }
 
   return (
     <div className="App">
       <div className='TopNav'>
         <h4>Blog</h4>
       </div>
-      <button onClick={()=>{
-        let copy = [...title];
-        copy.sort();
-        setTitle(copy)
-        }}>가나다순 정렬</button>
+      <button onClick={handleTitleSort
+      }>가나다순 정렬</button>
 
       {
         title.map((elem, i)=> {
         return (
           <div className='list' key={i}>
-            <h4>{elem}
-              <span onClick={()=>{setLike(like[i]+1)}}>👍🏻</span> {like[i]}
+            <h4 onClick={()=>{
+              setModal(!modal);
+              setTitleNum(i);
+              }}>{elem}
+              <span onClick={(e)=>{
+                e.stopPropagation();
+                console.log(e.target.key)
+                let copyLike = [...like];
+                copyLike[i] = copyLike[i]+1
+                setLike(copyLike)}}>👍🏻</span> {like[i]}
             </h4>
             <p>2월 17일 발행</p>
+            <button onClick={(e)=>{
+              let copy = [...title];
+              copy.splice(i,1)
+              setTitle(copy)
+            }}>삭제</button>
           </div>    
-        )})
-        }
-      {/* <div className='list'>
-        <h4 onClick={()=>{
+          
+              )})
+      }
+      <form>
+        Enter Title: <input onChange={(e)=>{setInputVal(e.target.value)}} required="required"></input>
+        <input type="submit" onClick={(e)=>{
+          e.preventDefault();
           let copy = [...title];
-          copy[0] = "여자코트추천";
+          copy.unshift(inputVal)
           setTitle(copy);
-        }}>{title[0]}<span onClick={handleLike}>👍🏻</span> {like} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{title[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4 onClick={()=>{setModal(!modal)}}>{title[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div> */}
+        }}></input>
+      </form>
       {
-        modal == true ? <Modal/> : null
+        modal == true ? <Modal title={title} style={{backgroundColor:"yellow"}} setTitle={setTitle} titleNum={titleNum}/> : null
       }
     </div>
   );
 }
-const Modal = () => {
+const Modal = (props) => {
   return (
-  <div className='modal'>
-    <h4>제목</h4>
+  <div className='modal' style={props.style}>
+    <h4>{props.title[props.titleNum]}</h4>
     <p>날짜</p>
     <p>상세내용</p>
+    <button onClick={()=>{
+      let copy = [...props.title];
+      copy[0] = '여자코트 추천';
+      props.setTitle(copy);
+    }}>글수정</button>
   </div>
   )
 }
